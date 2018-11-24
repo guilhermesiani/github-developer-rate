ebf159c6bcf50942678427afbe0219773c71c189
Guilherme Siani <guilherme@siani.com.br>
2018-11-24 10:35:49 -0200

    Binary compiled generated and validate auth

--- src/rate.go
+++ src/rate.go
@@ -3,6 +3,10 @@ package main
 import (
     "fmt"
     "flag"
+    "syscall"
+    "os"
+
+    "golang.org/x/crypto/ssh/terminal"
     "net/http"
     "io/ioutil"
 )
@@ -17,11 +21,27 @@ func main() {
 
     flag.Parse()
 
+    var user string;
+    fmt.Println("Github user: ")
+    fmt.Scanln(&user)
+
+    fmt.Println("Github password: ")
+    bytePassword, err := terminal.ReadPassword(int(syscall.Stdin))
+
+    if err != nil {
+        fmt.Println("Password can not be empty")
+        os.Exit(3)
+    }
+
+    fmt.Println()
+
     fmt.Println("Starting analyse")
 
     endpoint := fmt.Sprintf("https://api.github.com/repos/%s/%s/pulls", owner, repo)
-    fmt.Println(endpoint)
-    response, err := http.Get(endpoint)
+    request, err := http.NewRequest("GET", endpoint, nil)
+    request.SetBasicAuth(user, string(bytePassword))
+    client := &http.Client{}
+    response, err := client.Do(request)
 
     if err != nil {
         fmt.Println(fmt.Sprintf("The http request failed with error %s\n", err))
