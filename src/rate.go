63d976a698d14133e74537190a8ec9371c840073
Guilherme Siani <guilherme@siani.com.br>
2018-11-26 00:35:41 -0200

    Finished feature with some refactors

--- src/rate.go
+++ src/rate.go
@@ -3,13 +3,85 @@ package main
 import (
     "fmt"
     "flag"
+    "time"
     "syscall"
     "os"
-
+    "encoding/json"
     "golang.org/x/crypto/ssh/terminal"
     "net/http"
     "io/ioutil"
 )
+
+type PullRequest struct {
+    Id int
+    Url string
+    Number int
+    Title string
+    Created_at string
+    Updated_at string
+}
+
+type PullRequests struct {
+    Collection []PullRequest
+}
+
+type PullReview struct {
+    Id int
+    User  struct {
+        Id int
+        Login string
+    }
+}
+
+type PullReviews struct {
+    Collection []PullReview
+}
+
+func getPullRequests(dataType string, user string, password string, owner string, repo string) []PullRequest {
+
+    endpoint := fmt.Sprintf("https://api.github.com/repos/%s/%s/pulls", owner, repo)
+    request, err := http.NewRequest("GET", endpoint, nil)
+    request.SetBasicAuth(user, password)
+    client := &http.Client{}
+    response, err := client.Do(request)
+
+    if err != nil {
+        fmt.Println(fmt.Sprintf("The http request failed with error %s\n", err))
+        os.Exit(3)
+    }
+
+    data, _ := ioutil.ReadAll(response.Body)
+    jsonResponse := string(data)
+
+    keysBody := []byte(jsonResponse)
+    keys := make([]PullRequest, 0)
+    json.Unmarshal(keysBody, &keys)
+
+    return keys
+}
+
+func getPullReviews(dataType string, user string, password string, owner string, repo string, pullRequestNumber int) []PullReview {
+
+    endpoint := fmt.Sprintf("https://api.github.com/repos/%s/%s/pulls/%d/reviews", owner, repo, pullRequestNumber)
+    request, err := http.NewRequest("GET", endpoint, nil)
+    request.SetBasicAuth(user, password)
+    client := &http.Client{}
+    response, err := client.Do(request)
+
+    if err != nil {
+        fmt.Println(fmt.Sprintf("The http request failed with error %s\n", err))
+        os.Exit(3)
+    }
+
+    data, _ := ioutil.ReadAll(response.Body)
+    jsonResponse := string(data)
+
+    keysBody := []byte(jsonResponse)
+    keys := make([]PullReview, 0)
+    json.Unmarshal(keysBody, &keys)
+
+    return keys
+}
 
 func main() {
 
@@ -27,6 +99,7 @@ func main() {
 
     fmt.Println("Github password: ")
     bytePassword, err := terminal.ReadPassword(int(syscall.Stdin))
+    password := string(bytePassword)
 
     if err != nil {
         fmt.Println("Password can not be empty")
@@ -37,18 +110,64 @@ func main() {
 
     fmt.Println("Starting analyse")
 
-    endpoint := fmt.Sprintf("https://api.github.com/repos/%s/%s/pulls", owner, repo)
-    request, err := http.NewRequest("GET", endpoint, nil)
-    request.SetBasicAuth(user, string(bytePassword))
-    client := &http.Client{}
-    response, err := client.Do(request)
+    keys := getPullRequests("pullRequests", user, password, owner, repo)
+
+    var dateStart string;
+    fmt.Println("Date start: ")
+    fmt.Scanln(&dateStart)
+    dateStartParsed, err := time.Parse(time.RFC3339, dateStart)
 
     if err != nil {
         fmt.Println(fmt.Sprintf("The http request failed with error %s\n", err))
-    } else {
-        data, _ := ioutil.ReadAll(response.Body)
-        fmt.Println(string(data))
+        os.Exit(3)
     }
 
-    fmt.Println("Researchs ends", owner, repo)
+    var dateEnd string;
+    fmt.Println("Date end: ")
+    fmt.Scanln(&dateEnd)
+    dateEndParsed, err := time.Parse(time.RFC3339, dateEnd)
+
+    if err != nil {
+        fmt.Println(fmt.Sprintf("The http request failed with error %s\n", err))
+        os.Exit(3)
+    }
+
+    var githubUser string;
+    fmt.Println("Github user to calculate: ")
+    fmt.Scanln(&githubUser)
+
+    pullRequestsCount := 0
+    pullReviewsCount := 0
+
+    fmt.Println()
+    fmt.Println("Calculating ")
+
+    for i := 0; i < len(keys); i++ {
+        pullRequestsCount++
+
+        update, err := time.Parse(time.RFC3339, keys[i].Created_at)
+        if err != nil {
+            fmt.Println(fmt.Sprintf("The http request failed with error %s\n", err))
+            os.Exit(3)
+        }
+        if dateStartParsed.After(update) || dateEndParsed.Before(update) {
+            continue
+        }
+
+        reviewKeys := getPullReviews("pullRequests", user, password, owner, repo, keys[i].Number)
+
+        for j := 0; j < len(reviewKeys); j++ {
+            if reviewKeys[j].User.Login == githubUser {
+                pullReviewsCount++
+            }
+        }
+        fmt.Print(".")
+    }
+
+    fmt.Println()
+    fmt.Println()
+    fmt.Printf("User %s did %d reviews about %d pull requests on interval between %s and %s", githubUser, pullReviewsCount, pullRequestsCount, dateStart, dateEnd)
+
+    fmt.Println()
+    fmt.Println("Researchs ends for", owner, repo)
 }
